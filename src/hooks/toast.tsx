14399e204f910e35d87ddf88808240d53c24c6ec
FelipeRodrigues1337 <proen_dev@hotmail.com>
2021-11-29 09:18:11 -0300

    configuração para deploy

---
+++ src/hooks/toast.tsx
@@ -0,0 +1,61 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import ToastContainer from '../components/ToastContainer/index';
+import { v4 as uuid } from 'uuid';
+
+export interface ToastMessage {
+    id: string;
+    type?: 'info' | 'success' | 'error';
+    title: string;
+    description?: string;
+  }
+  
+
+interface ToastContextData {
+    addToast(messages: Omit<ToastMessage, 'id'>): void;
+    removeToast(id: string): void;
+}
+
+const ToastContext = createContext<ToastContextData>({} as ToastContextData);
+
+const ToastProvider: React.FC = ({ children }) => {
+
+    const [messages, setMessages] = useState<ToastMessage[]>([]);
+
+    const addToast = useCallback(({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+        const id = uuid();
+        
+        const toast = {
+            id,
+            type,
+            title,
+            description,
+        };
+
+        setMessages(oldMessages => [...oldMessages, toast]);
+
+    }, [messages]);
+
+    const removeToast = useCallback((id: string) => {
+       setMessages(state => state.filter(message => message.id !== id));
+    }, []);
+
+    return (
+        <ToastContext.Provider value={{ addToast, removeToast }}>
+            {children}
+        <ToastContainer message={messages}/>
+
+        </ToastContext.Provider>
+    );
+}
+
+function useToast(): ToastContextData {
+    const context = useContext(ToastContext);
+
+    if (!context) {
+        throw new Error('useToast must be used within a ToastProvider')
+    }
+
+    return context;
+}
+
+export { ToastProvider, useToast };
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,100 @@
+import React, { useCallback, useRef } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link, useHistory} from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+import logoweb from '../../assets/logo-web.png'
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignInFormData {
+    email: string,
+    password: string,
+}
+
+const Signin: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+
+    const { signIn } = useAuth();
+    const { addToast } = useToast();
+
+    const history = useHistory();
+
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
+        try {
+            formRef.current?.setErrors({});
+
+            const schema = Yup.object().shape({
+                email: Yup.string().required('E-mail obrigatório').required('E-mail obrigatório'),
+                password: Yup.string().required('Senha obrigatória'),
+            });
+
+            await schema.validate(data, { abortEarly: false });
+
+            await signIn({
+                email: data.email,
+                password: data.password,
+            });
+
+            history.push('/dashboard');
+
+        } catch (err) {
+
+            if (err instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(err);
+
+                formRef.current?.setErrors(errors);
+
+                return;
+            }
+
+            addToast({
+                type: 'error',
+                title: 'Erro na autenticação',
+                description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
+            });
+        }
+    }, [signIn, addToast, history],
+    );
+
+    return (
+        <Container>
+            <Content>
+                <AnimationContainer>
+                    <img src={logoweb} />
+
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu login</h1>
+
+                        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            type="password"
+                            placeholder="Senha"
+                        />
+
+                        <Button type="submit">Entrar</Button>
+
+                        <Link to="/forgot-password">Esqueci minha senha</Link>
+                    </Form>
+
+                    <Link to="/signup"><FiLogIn />Criar conta</Link>
+                    
+                </AnimationContainer>
+            </Content>
+            <Background />
+        </Container>
+    )
+}
+
+export default Signin;
